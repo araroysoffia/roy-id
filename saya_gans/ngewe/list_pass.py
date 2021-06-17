372fb37a4e789e303543bf71f53f91fe776e1f12
royofficial <85897883+royofficial@users.noreply.github.com>
2021-06-18 01:29:59 +0700

    Create list_pass.py

---
+++ saya_gans/ngewe/list_pass.py
@@ -0,0 +1,21 @@
+# coding:utf-8
+
+pw_tambahan="""
+sayang|bismillah
+""".strip().split("|")
+ngntd=((())>(()))+((())>(()))
+def awok(ngentod):
+	return [ngentod,ngentod+"123",ngentod+"12345"]
+def pw_list(ngentot):
+	if len(ngentot)==2:
+		romi=ngentot[1].split(" ")
+		if len(romi[ngntd])!=ngntd:
+			asu=awok(romi[ngntd].lower())
+			if len(romi[ngntd]) < 6:
+				del asu[ngntd]
+				[asu.append(x) for x in pw_tambahan]
+			else:
+				[asu.append(x) for x in pw_tambahan]
+		else: asu=pw_tambahan
+	else: asu=pw_tambahan
+	return asu
