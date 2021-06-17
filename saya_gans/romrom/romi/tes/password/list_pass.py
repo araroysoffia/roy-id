9d601d07a61cc0534bf694b0d60bbbc0efe3d346
royofficial <85897883+royofficial@users.noreply.github.com>
2021-06-18 01:39:00 +0700

    Create list_pass.py

---
+++ saya_gans/romrom/romi/tes/password/list_pass.py
@@ -0,0 +1,20 @@
+# Silahkan Jika Ingin Mengubah Listpass Nya
+pw_tambahan="""
+sayang|bismillah
+""".strip().split("|")
+ngntd=((())>(()))+((())>(()))
+def awok(ngentod):
+	return [ngentod+"123",ngentod+"12345"]
+def pw_list(ngentot):
+	if len(ngentot)==2:
+		rom_mi=ngentot[1].split(" ")
+		if len(rom_mi[ngntd])!=ngntd:
+			asu=awok(rom_mi[ngntd])
+			if len(rom_mi[ngntd])<=5:
+				del asu[ngntd]
+				for x in pw_tambahan: asu.append(x)
+			else:
+				for x in pw_tambahan: asu.append(x)
+		else: asu=pw_tambahan
+	else: asu=pw_tambahan
+	return asu
